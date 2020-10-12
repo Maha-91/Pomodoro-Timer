1413d054416931d1de7da3b60229cc258f0b71c7
Maha Jey <jmahabe@gmail.com>
2020-10-11 20:49:39 -0400

    adding shortbreak html file

---
+++ pomodoro.js
@@ -0,0 +1,6 @@
+function shortbreak() {
+  location.replace("http://127.0.0.1:5500/Pomodoro-Timer/Shortbreak.html");
+}
+function Pomodoro() {
+  location.replace("http://127.0.0.1:5500/Pomodoro-Timer/Pomodoro.html");
+}
