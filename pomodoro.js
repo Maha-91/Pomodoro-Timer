83180a0979b537987e7aa97602c22fabbd7488ef
Maha Jey <jmahabe@gmail.com>
2020-10-14 17:30:57 -0400

    ok

--- pomodoro.js
+++ pomodoro.js
@@ -4,3 +4,6 @@ function shortbreak() {
 function Pomodoro() {
   location.replace("http://127.0.0.1:5500/Pomodoro-Timer/Pomodoro.html");
 }
+function longbreak() {
+  location.replace("http://127.0.0.1:5500/Pomodoro-Timer/longbreak.html");
+}
